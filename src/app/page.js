bba44565c0d9a3aa6fca2c7b77cf9e2792065b50
stanleyos <stanislaw.sarka@gmail.com>
2023-12-28 16:52:56 +0100

    Updated README

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex   justify-center ">
+    <main className="flex justify-center ">
       <h1 className="text-3xl my-12"> Willa Hanusia</h1>
     </main>
   );
