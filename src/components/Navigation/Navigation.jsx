3ea34a2bede29791199e559f4f9c712fc4a33c7f
stanleyos <stanislaw.sarka@gmail.com>
2023-12-28 16:40:55 +0100

    Implemented responsive navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,15 @@
 "use client";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import styles from "./navigation.module.scss";
 import Logo from "../Logo/Logo";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [showHamburger, setShowHamburger] = useState(false);
+  console.log(showHamburger);
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
@@ -16,7 +20,7 @@ const Navigation = () => {
       <Link href="/">
         <Logo />
       </Link>
-      <ul>
+      <ul className={styles.navigationList}>
         <li>
           <Link href="/onas">O nas</Link>
         </li>
@@ -30,6 +34,27 @@ const Navigation = () => {
           <Link href="/kontakt">Kontakt</Link>
         </li>
       </ul>
+      <div className={styles.navigationHamburger}>
+        <div
+          className={` ${showHamburger ? "mobileNavigationList" : "hidden"}`}
+        >
+          <ul>
+            <li>
+              <Link href="/onas">O nas</Link>
+            </li>
+            <li>
+              <Link href="/galeria">Galeria</Link>
+            </li>
+            <li>
+              <Link href="/oferta">Oferta</Link>
+            </li>
+            <li>
+              <Link href="/kontakt">Kontakt</Link>
+            </li>
+          </ul>
+        </div>
+        <GiHamburgerMenu onClick={() => setShowHamburger(!showHamburger)} />
+      </div>
     </motion.div>
   );
 };
