afc6f72f7755e1cb3f358cbfe2c602395301db98
stanleyos <stanislaw.sarka@gmail.com>
2023-12-31 11:52:46 +0100

    Fix iframe error in console

--- src/app/kontakt/page.jsx
+++ src/app/kontakt/page.jsx
@@ -34,7 +34,6 @@ const KontaktPage = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10840.807428944843!2d19.931135128561746!3d49.6046591179842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4715e021ea6a1ac1%3A0xa9eb75a49d1675f4!2sWilla%20Hanusia!5e1!3m2!1spl!2spl!4v1703950100013!5m2!1spl!2spl"
             width="600"
             height="450"
-            allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
