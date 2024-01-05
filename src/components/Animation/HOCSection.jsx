d5f2cc4afbc070a649a082ac3b57436f9e9b9008
stanleyos <stanislaw.sarka@gmail.com>
2024-01-05 17:52:01 +0100

    Fixed view width problem

--- src/components/Animation/HOCSection.jsx
+++ src/components/Animation/HOCSection.jsx
@@ -3,15 +3,14 @@ import { useRef } from "react";
 import { useInView } from "framer-motion";
 import styles from "./section.module.scss";
 
-const HOCSection = ({ children, side }) => {
+const HOCSection = ({ children }) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
-  //let direction = side == "left" ? "translateX(-100px)" : "translateX(100px)";
   return (
     <section className={styles.sectionContainer} ref={ref}>
       <span
         style={{
-          transform: isInView ? "none" : "",
+          transform: isInView ? "none" : "translateX(-100px)",
           opacity: isInView ? 1 : 0,
           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}
--- src/components/common/Section/SectionComponent.jsx
+++ src/components/common/Section/SectionComponent.jsx
@@ -4,15 +4,14 @@ import { useInView } from "framer-motion";
 
 import styles from "./section.module.scss";
 
-const SectionComponent = ({ children, idName, side }) => {
+const SectionComponent = ({ children, idName }) => {
   const ref = useRef(null);
-  const isInView = useInView(ref);
-  let direction = side == "left" ? "translateX(-100px)" : "translateX(100px)";
+  const isInView = useInView(ref, { once: true });
   return (
     <section id={idName} className={styles.sectionDiv} ref={ref}>
       <span
         style={{
-          transform: isInView ? "none" : direction,
+          transform: isInView ? "none" : "translateY(400px)",
           opacity: isInView ? 1 : 0,
           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
         }}
