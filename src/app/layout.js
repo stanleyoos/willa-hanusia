d86730b7c3c223adaadcdfb4e94abba2f8e072af
stanleyos <stanislaw.sarka@gmail.com>
2024-01-08 13:53:03 +0100

    Styling fixes in About us section, compressed the logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import Footer from "@/components/Footer/Footer";
 import Container from "@/components/Container/Container";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 const lato = Lato({ weight: "400", style: "italic", subsets: ["latin"] });
 
 export const metadata = {
--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -7,25 +7,30 @@ const AboutUs = () => {
     <>
       <h1 className="text-7xl my-12 text-center">O nas</h1>
       <div className={styles.divContainer}>
-        <div>
+        <div className={styles.leftDiv}>
           <ul className="mb-5">
             <li>- Rodzinna atmosfera</li>
             <li>- Domowe wyżywienie</li>
             <li>- Kryty plac zabaw</li>
             <li>- Cicha i spokojna okolica</li>
           </ul>
-          to elementy dzięki którym wypoczynek będzie naprawdę udany. Wszystkie
-          z nich znajdziecie Państwo w Willi Hanusia - najlepszym miejscu do
-          wypoczynku z dziećmi.
+          <div>
+            {/* to elementy dzięki którym wypoczynek będzie naprawdę udany. */}
+            Wszystko to znajdziecie Państwo w Willi Hanusia - najlepszym miejscu
+            do wypoczynku z dziećmi.
+          </div>
         </div>
-        <iframe
-          width="560"
-          height="415"
-          src="https://www.youtube.com/embed/v3FHEiDZhzI?si=MSS0QnvJtWxzCMge"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div>
+          <h1 className="text-xl mb-2">Zobacz film:</h1>
+          <iframe
+            // width="560"
+            // height="415"
+            src="https://www.youtube.com/embed/v3FHEiDZhzI?si=MSS0QnvJtWxzCMge"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
       <h1 className="text-3xl text-center px-3 mb-10">
         Zapraszamy do zapoznania się z naszą ofertą
