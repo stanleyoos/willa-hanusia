55f252fdab6393e2ef62ef1df5294215f5c458c4
stanleyos <stanislaw.sarka@gmail.com>
2024-01-09 12:48:30 +0100

    Styled Kontakt section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ import { IoIosMail } from "react-icons/io";
 const Contact = () => {
   return (
     <>
-      <h1 className="text-5xl text-center">Kontakt</h1>
+      <h1 className="text-7xl text-center my-12">Kontakt</h1>
       <div className={styles.divContainer}>
         <div className={styles.informations}>
           <div>
@@ -18,7 +18,7 @@ const Contact = () => {
           </div>
           <div>
             <FaPhoneAlt />
-            <h1>Kontakt</h1>
+            <h1>Telefon</h1>
             <p className="my-3">
               <a href="tel:18 26 77 547">18 26 77 547</a>
             </p>
@@ -44,15 +44,14 @@ const Contact = () => {
           <p>Doba hotelowa: od 14:00 do 11:00 dnia następnego</p>
           <p>Na zapytania mailowe odpowiadamy do 24 godzin</p>
         </div>
-        {/* <div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10840.807428944843!2d19.931135128561746!3d49.6046591179842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4715e021ea6a1ac1%3A0xa9eb75a49d1675f4!2sWilla%20Hanusia!5e1!3m2!1spl!2spl!4v1703950100013!5m2!1spl!2spl"
-            width="600"
-            height="450"
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div> */}
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10840.807428944843!2d19.931135128561746!3d49.6046591179842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4715e021ea6a1ac1%3A0xa9eb75a49d1675f4!2sWilla%20Hanusia!5e1!3m2!1spl!2spl!4v1703950100013!5m2!1spl!2spl"
+          width="600"
+          height="250"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </>
   );
